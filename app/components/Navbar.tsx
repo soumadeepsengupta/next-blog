4da4e54ae3a0a8f3c982dae0137e63238e0ac826
soumadeepsengupta <soumadeep.sengupta11032004@gmail.com>
2025-10-31 02:33:13 +0530

    Theming and fonts done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-5 py-3 bg-white font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
