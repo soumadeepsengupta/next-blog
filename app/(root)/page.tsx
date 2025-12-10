71a93b79031b338e8f0cc8fd2682027c705b99ce
soumadeepsengupta <soumadeep.sengupta11032004@gmail.com>
2025-12-10 17:43:13 +0530

    Hero section added with search input

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+// import React from 'react'
+// import SearchForm from '@/components/SearchForm';
+// import Link from 'next/link'
+// import Image from 'next/image'
 
-const page = () => {
+// export default async function Home({
+//   searchParams,
+// }: {
+//   searchParams: Promise<{ query?: string }>;
+// }) {
+//   const query = (await searchParams).query;
+
+//       <section className="red-container _pattern">
+//         <span className="tag">
+//           Pitch, Vote, and Grow
+//         </span>
+//         <h1 className="heading">
+//           Pitch Your Startup, <br />
+//           Connect With Entrepreneurs
+//         </h1>
+
+//         <p className="sub-heading !max-w-3xl">
+//           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+//           Competitions.
+//         </p>
+        
+//         <SearchForm query={query} />
+//       </section>
+// }
+
+
+
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
+
   return (
       <section className="red-container _pattern">
-        <span className="tag">
+        <span className="tag tag-tri">
           Pitch, Vote, and Grow
         </span>
         <h1 className="heading">
@@ -17,9 +54,8 @@ const page = () => {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
+        
+        <SearchForm query={query} />
       </section>
-  )
+  );
 }
-
-export default page
-
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
+          <Image src="/Logo.svg" alt="logo" width={144} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
