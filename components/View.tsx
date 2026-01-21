ac1be9da8d77430e020b6b4cc2e85b6042540055
soumadeepsengupta <soumadeep.sengupta11032004@gmail.com>
2026-01-22 02:30:37 +0530

    Views updated using write-client and ppr working

--- components/View.tsx
+++ components/View.tsx
@@ -1,27 +1,32 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
-import { Eye } from "lucide-react";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  // TODO: Update the number of views
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit(),
+  );
 
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping />
       </div>
+
       <p className="view-text">
-        <span className="font-bold">
-          <Eye className="inline-block mr-2" />
-          Views: {totalViews}</span>
+        <span className="font-black">Views: {totalViews}</span>
       </p>
     </div>
   );
 };
-
 export default View;
--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -21,7 +22,42 @@ const nextConfig: NextConfig = {
   },
 };
 
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
+  org: "js-mastery-dk",
+  project: "javascript-nextjs",
 
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-export default nextConfig;
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Hides source maps from generated client bundles
+  sourcemaps: {
+    disable: false,
+  },
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
+
+  // Use webpack configuration for new Sentry options
+  webpack: {
+    treeshake: {
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      removeDebugLogging: true,
+    },
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    automaticVercelMonitors: true,
+  },
+});
